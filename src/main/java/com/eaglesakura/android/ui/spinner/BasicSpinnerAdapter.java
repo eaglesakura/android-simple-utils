a8e049f9ec2bc5775c70e35ae29a34a9aab044ec
eaglesakura <eagle.sakura@gmail.com>
2016-08-04 14:49:12 +0900

    アイテムカラーを変更できるように機能追加

--- src/main/java/com/eaglesakura/android/ui/spinner/BasicSpinnerAdapter.java
+++ src/main/java/com/eaglesakura/android/ui/spinner/BasicSpinnerAdapter.java
@@ -1,6 +1,7 @@
 package com.eaglesakura.android.ui.spinner;
 
 import android.content.Context;
+import android.support.annotation.ColorInt;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,7 +12,10 @@ import android.widget.TextView;
  * Spinner向けのAdapter
  */
 public class BasicSpinnerAdapter extends ArrayAdapter<String> {
-    boolean textRight = false;
+    boolean mTextRight = false;
+
+    @ColorInt
+    int mItemTextColor;
 
     public BasicSpinnerAdapter(Context context) {
         super(context, android.R.layout.simple_spinner_item);
@@ -19,13 +23,22 @@ public class BasicSpinnerAdapter extends ArrayAdapter<String> {
     }
 
     public void setTextRight(boolean textRight) {
-        this.textRight = textRight;
+        this.mTextRight = textRight;
+    }
+
+    /**
+     * テキスト色を指定する
+     *
+     * @param itemTextColor ARGB色
+     */
+    public void setItemTextColor(@ColorInt int itemTextColor) {
+        mItemTextColor = itemTextColor;
     }
 
     @Override
     public View getDropDownView(int position, View convertView, ViewGroup parent) {
         View result = super.getDropDownView(position, convertView, parent);
-        if (textRight) {
+        if (mTextRight) {
             ((TextView) result.findViewById(android.R.id.text1)).setGravity(Gravity.END);
         }
         return result;
@@ -34,8 +47,13 @@ public class BasicSpinnerAdapter extends ArrayAdapter<String> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View result = super.getView(position, convertView, parent);
-        if (textRight) {
-            ((TextView) result.findViewById(android.R.id.text1)).setGravity(Gravity.END);
+
+        TextView view = ((TextView) result.findViewById(android.R.id.text1));
+        if (mTextRight) {
+            view.setGravity(Gravity.END);
+        }
+        if (mItemTextColor != 0) {
+            view.setTextColor(mItemTextColor);
         }
         return result;
     }
